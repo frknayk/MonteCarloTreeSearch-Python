6b1af4aab09e3c50496ed94bdb1d4a139ced5ab2
someone <you@example.com>
2020-09-19 17:34:09 +0300

    connect_four is completed with render only mode

--- Games/enums/colors.py
+++ Games/enums/colors.py
@@ -5,4 +5,6 @@ class Colors(Enum):
     BLUE = (0,0,255)
     BLACK = (0,0,0)
     RED = (255,0,0)
+    GREEN = (0,255,0)
+    PURPLE = (255, 51, 153)
     YELLOW = (255,255,0)
--- Games/game_maker.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-class ConnectEnv:
-    def __init__(self):
-        pass
