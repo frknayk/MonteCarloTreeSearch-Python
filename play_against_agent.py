134e5f5bc9f5e4606df11921f5a794ba316f0e8c
someone <you@example.com>
2020-10-18 19:19:46 +0300

    Rendered mode is added to play agains ai

--- play_against_agent.py
+++ play_against_agent.py
@@ -10,10 +10,20 @@ root = Node(gym_env=env_train, game_status=RESULTS.NOT_FINISHED, move=None, pare
 mcts_agent = MonteCarloTreeSearch(root)
 mcts_agent.load('TrainedAgents/mcts_brain_200.pickle')
 
+
 is_done = False
 root_node = mcts_agent.root
 env_test = gym.make('connect-v0')
+
+# Enable debug mode for rendering mode
+from gym_connect.envs.enums.run_mode import MODE
+env_test.mode_game = MODE.RENDER_DEBUG
+env_test.set_renderer()
+
 while not is_done:
+    
+    env_test.render()
+
     if env_test.PLAYER is PLAYER.FIRST:
         selected_action = env_test.get_action_from_terminal()
         # selected_action = env_test.get_random_action()
@@ -31,7 +41,7 @@ while not is_done:
         # If its an unexplored state
         if selected_action is None:
             mcts_agent = MonteCarloTreeSearch(root_node)
-            mcts_agent.train(training_step=15)
+            mcts_agent.train(training_step=1)
             root_node = mcts_agent.root.copy()
             selected_action = root_node.select_move()
         _, _, is_done = env_test.step(selected_action)
@@ -41,7 +51,7 @@ while not is_done:
             print("WRONG STATE ASSIGNMENT !")
             break
         root_node = node_selected
-
+    
     print(root_node.env.state)
 
 print("Winning player : ",root_node.env.PLAYER)
